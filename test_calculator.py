88b3f816e8c7077e4978ceda3cdb5d0f8fd83ea7
Jacob Helfman <jacobjhelfman@gmail.com>
2021-11-09 20:41:42 -0800

    Made flake8 fixes

--- test_calculator.py
+++ test_calculator.py
@@ -2,15 +2,17 @@
 Calculator library containing basic math operations.
 """
 # The test_ at the start of this file name lets pytest know this is a test
-# Run the following --> The . is to test only the test_ file (not the venv folder!):  pytest -v --cov . (remember,   -v, --verbose         increase verbosity.)
+# Run the following -->
+# The . is to test only the test_ file (not the venv folder!):
+# pytest -v --cov . (remember,   -v, --verbose         increase verbosity.)
 
 import calculator
 
 
 class TestCalculator:
-    
-        def test_addition(self):
-            assert 4 == calculator.add(2, 2)
-            
-        def test_subtract(self):
-            assert 2 == calculator.subtract(4, 2)
+
+    def test_addition(self):
+        assert 4 == calculator.add(2, 2)
+
+    def test_subtract(self):
+        assert 2 == calculator.subtract(4, 2)
